ce6c32d8991524860b2eab82d5986add5a082319
BAe <bae0712bar@gmail.com>
2021-04-18 21:52:24 +0800

    third

--- read.py
+++ read.py
@@ -4,7 +4,7 @@ with open('reviews.txt', 'r') as f:
  	for line in f:
  		data.append(line)
  		count += 1 #count = count + 1
- 		if count % 10000 == 0:
+ 		if count % 1000 == 0:
  			print(len(data))
  		
 print('總共讀取', len(data), '筆資料')
@@ -12,11 +12,24 @@ print('總共讀取', len(data), '筆資料')
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
-	
 print('留言平均長度為', sum_len / len(data))
 
+new = [] 
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('總共有', len(d), '筆留言長度小於100')
+print(new[0])
+print(new[1])
 
+good = []
+for d in data:
+	if 'good' in data:
+		good.append(d)
+print('總共有', len(good), '筆留言提到good')
 
-
-
-
+hi = []
+for d in data:
+	if 'hi' in data:
+		hi.append(d)
+print('總共有', len(good), '筆留言提到hi')
